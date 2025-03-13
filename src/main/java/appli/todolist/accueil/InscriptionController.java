f2e78a97f1eb3b8d246557aa2fbf0faac7337878
JBAO <baojonathan95@gmail.com>
2025-03-13 01:45:41 +0100

    Import des attributs et methodes pour Inscription.Controller

--- src/main/java/appli/todolist/accueil/InscriptionController.java
+++ src/main/java/appli/todolist/accueil/InscriptionController.java
@@ -1,4 +1,38 @@
 package appli.todolist.accueil;
 
+
+    import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class InscriptionController {
-}
+
+        @FXML
+        private Button boutonInscriptionInscrip;
+
+        @FXML
+        private Label boutonLabelErreurInscrip;
+
+        @FXML
+        private Button boutonRetour;
+
+        @FXML
+        private PasswordField confirmationMDP;
+
+        @FXML
+        private TextField emailCaseInscrip;
+
+        @FXML
+        private PasswordField mdpCaseInscrip;
+
+        @FXML
+        private TextField nomCase;
+
+        @FXML
+        private TextField prenomCase;
+
+    }
+
+
